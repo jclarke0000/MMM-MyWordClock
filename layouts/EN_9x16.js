7c7ccc69c949d14bae0a989b4a145d10660c1a85
Jeff Clarke <jeff.clarke@kineticsocial.com>
2017-08-16 14:47:40 -0400

    Replaced Lobster font with the Google Fonts version which supports accented characters.

--- layouts/EN_9x16.js
+++ layouts/EN_9x16.js
@@ -5,7 +5,7 @@ var layout = {
   config : [
     [
       {
-        word: "It's",
+        word: "It’s",
         minutes: [0,5,10,15,20,25,30,35,40,45,50,55,60]
       },
       {
@@ -103,7 +103,7 @@ var layout = {
         hours: [0,12] 
       },
       {
-        word: "o'clock",
+        word: "o’clock",
         minutes: [0] 
       }
     ]
