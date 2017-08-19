4a4a63273ca7513196456b96c3dccbf54bbe0e15
fewieden <fewieden@htwg-konstanz.de>
2017-08-19 09:37:38 +0200

    add swissgerman tall version

--- layouts/DE_CH_9x16.js
+++ layouts/DE_CH_9x16.js
@@ -1,6 +1,6 @@
 var layout = {
-    name: "DE_CH_16x9",
-    classes: "wide",
+    name: "DE_CH_9x16",
+    classes: "tall",
     nextHourAt: 25,
     config : [
         [
@@ -13,15 +13,15 @@ var layout = {
                 minutes: [15,45]
             },
             {
-                word: "füf",
-                minutes: [5,25,35,55]
-            },
-            {
                 word: "zä",
                 minutes: [10,40,50]
             }
         ],
         [
+            {
+                word: "füf",
+                minutes: [5,25,35,55]
+            },
             {
                 word: "zwanzg",
                 minutes: [20]
@@ -29,7 +29,9 @@ var layout = {
             {
                 word: "vor",
                 minutes: [25,45,50,55]
-            },
+            }
+        ],
+        [
             {
                 word: "ab",
                 minutes: [5,10,15,20,35,40]
@@ -37,13 +39,13 @@ var layout = {
             {
                 word: "halbi",
                 minutes: [25,30,35,40]
-            }
-        ],
-        [
+            },
             {
                 word: "eis",
                 hours: [1,13]
-            },
+            }
+        ],
+        [
             {
                 word: "zwöi",
                 hours: [2,14]
@@ -53,44 +55,46 @@ var layout = {
                 hours: [3,15]
             },
             {
-                word: "sibni",
-                hours: [7,19]
+                word: "sächsi",
+                hours: [6,18]
             }
         ],
         [
             {
-                word: "sächsi",
-                hours: [6,18]
+                word: "sibni",
+                hours: [7,19]
+            },
+            {
+                word: "vieri",
+                hours: [4,16]
+            },
+            {
+                word: "füfi",
+                hours: [5,17]
+            }
+        ],
+        [
+            {
+                word: "achti",
+                hours: [8,20]
             },
             {
                 word: "nüni",
                 hours: [9,21]
             },
             {
-                word: "füfi",
-                hours: [5,17]
-            },
-            {
-                word: "zwöufi",
-                hours: [0,12]
+                word: "zäni",
+                hours: [10,22]
             }
         ],
         [
-            {
-                word: "vieri",
-                hours: [4,16]
-            },
-            {
-                word: "zäni",
-                hours: [10,22]
-            },
             {
                 word: "eufi",
                 hours: [11,23]
             },
             {
-                word: "achti",
-                hours: [8,20]
+                word: "zwöufi",
+                hours: [0,12]
             }
         ]
     ]
